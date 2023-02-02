4d466428aa6416608369c0df0e7abaaf12d3f390
alexsans12 <eddalexcab@gmail.com>
2023-02-02 16:55:25 -0600

    Changed the way to show the thank you card

--- js/app.js
+++ js/app.js
@@ -1,13 +1,9 @@
-/*
-
-You selected
-<!-- Add rating here -->
-out of 5 Thank you! We appreciate you taking the time to give a rating.
-If you ever need more support, don’t hesitate to get in touch!
-
-*/
 const form = document.querySelector('#form-rate');
 form.addEventListener('submit', checkRate);
+
+const cardRate = document.querySelector('.card-rate');
+const cardTy = document.querySelector('.card-rate--center');
+
 
 function checkRate(evt) {
     evt.preventDefault();
@@ -15,28 +11,11 @@ function checkRate(evt) {
     const radioBtn = document.querySelector('.card-rate__radio:checked');
 
     if(radioBtn) {
-        const cardRate = document.querySelector('.card-rate');
-        cardRate.classList.add('animation-out');
+        cardRate.classList.add('hidden');
 
-        const card = document.createElement('article');
-        card.classList.add('card-rate','card-rate--center');
+        document.querySelector('.card_rate__badge').textContent = `You selected ${radioBtn.value} out 5`;
 
-        card.innerHTML = `
-            <header class="card-rate__header">
-                <img class="card-rate__header-img" src="./images/illustration-thank-you.svg" alt="star icon"/>
-                <label class="card_rate__badge">You selected ${radioBtn.value} out of 5</label>
-                <h2 class="card-rate__title">Thank you!</h2>
-            </header>
-            <main class="card-rate__main">
-                <p class="card-rate__paragraph">We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!</p>
-            </main>
-        `;
-
-        
-        setTimeout(() => {
-            document.querySelector('body').appendChild(card);
-            card.classList.add('animation-in');
-            cardRate.remove();
-        }, 100);
+        cardTy.classList.remove('hidden');
+        cardTy.classList.add('animation-in');
     }
 }
